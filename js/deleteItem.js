68ef9b195e789c48bce30ce8cc409cba3d7b6f3b
SerineMarie <71319320+SerineMarie@users.noreply.github.com>
2021-06-03 23:04:20 +0200

    created responsive layout

--- js/deleteItem.js
+++ js/deleteItem.js
@@ -1,17 +1,17 @@
-const deleteItemCart = document.querySelectorAll(".delete")
+const deleteItemCart = document.querySelectorAll("#delete")
 
 for (let i = 0; i < deleteItemCart.length; i++){
     const deletebutton = deleteItemCart[i]
     deletebutton.addEventListener("click", function(event){
         const deletebuttonClicked = event.target
-        deletebuttonClicked.parentElement.parentElement.remove();
+        deletebuttonClicked.parentElement.remove();
         cartTotal();
     })
 }
 
 function cartTotal(){
-    const cartItems = document.querySelectorAll("#cartItems")[0]
-    const cartItem = document.querySelectorAll(".cartItem")
+    const cartItems = document.querySelector(".productsContainer")[0]
+    const cartItem = document.querySelectorAll(".products")
     for(let i = 0; i < cartItem.length; i++){
         const totalCartItem = cartItem[i]
     }
